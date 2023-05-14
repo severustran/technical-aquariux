b2e41e16ad95bbe52f1f09f8648e2a027adbe349
Liem Tran <liem.tran@antnest.vn>
2023-05-14 11:28:17 +0700

    init project

---
+++ discovery-service/src/test/java/com/aquariux/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aquariux.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ wallet-service/src/test/java/com/aquariux/walletservice/WalletServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aquariux.walletservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
